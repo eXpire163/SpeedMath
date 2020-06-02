fa518241f1ffc12844a311b044040227dbde18fb
eXpire163 <git@mms2k.de>
2020-06-02 13:26:47 +0200

    new colors

---
+++ Assets/ColorHelper.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorHelper : MonoBehaviour
+{
+
+   public static Color32[] lightColors = new Color32[] {
+        new Color32(85, 239, 196,255),
+        new Color32(129, 236, 236,255),
+        new Color32(116, 185, 255, 255),
+        new Color32(162, 155, 254, 255),
+        new Color32(253, 121, 168, 255),
+        new Color32(255, 118, 117, 255),
+        new Color32(250, 177, 160, 255),
+        new Color32(255, 234, 167, 255),
+        new Color32(223, 230, 233, 255),
+        new Color32(99, 110, 114, 255)
+    };
+    public static Color32[] darkColors = new Color32[] {
+           new Color32(0, 184, 148, 255),
+           new Color32(0, 206, 201, 255),
+           new Color32(9, 132, 227, 255),
+           new Color32(108, 92, 231, 255),
+           new Color32(232, 67, 147, 255),
+           new Color32(214, 48, 49, 255),
+           new Color32(225, 112, 85, 255),
+           new Color32(253, 203, 110, 255),
+           new Color32(178, 190, 195, 255),
+           new Color32(45, 52, 54,255)
+    };
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/LaneSettings.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaneSettings : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    
+    public GameObject box;
+
+    public int colorID = 9;
+
+    void Start()
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        SpriteRenderer srbox = box.GetComponent<SpriteRenderer>();
+
+
+        sr.color = ColorHelper.lightColors[colorID];
+        srbox.color = ColorHelper.darkColors[colorID];
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
